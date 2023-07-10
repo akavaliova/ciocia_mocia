ac046709e2a91627b2899b86cbef1b63aa0f7551
akavaliova <kovaleva89alena@gmail.com>
2023-07-10 12:41:38 +0200

    sass instalation

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './App.scss'
+import styles from './Home.module.scss'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      <div className={styles.container}>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
