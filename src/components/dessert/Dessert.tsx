084ad7b5771f74fe40eb96f3b67c8c9633871a4e
akavaliova <kovaleva89alena@gmail.com>
2023-07-21 15:51:18 +0200

    render desserts with map, using interface

--- src/components/dessert/Dessert.tsx
+++ src/components/dessert/Dessert.tsx
@@ -1,18 +1,15 @@
 import "./style.scss";
-import Blackcurrant from "./../../img/photos/Blackcurrant.jpg";
+// import Blackcurrant from "./../../img/photos/Blackcurrant.jpg";
+import {DessertItem} from "./../../pages/Desserts";
 
-interface DessertProps{
-  title: string;
-}
 
-const Dessert = ({title}:DessertProps) => {
+const Dessert = ({title, img}: DessertItem) => {
   return (
     <li className="dessert">
-      <a href="./dessert-page.html">
-        <img src={Blackcurrant} alt="Dessert img" className="dessert__img" />
-        {/* <h3 className="dessert__title">Blackcurrant Mochi</h3> */}
+      {/* <a href="./dessert-page.html"> */}
+        <img src={img} alt="Dessert img" className="dessert__img" />
         <h3 className="dessert__title">{title}</h3>
-      </a>
+      {/* </a> */}
     </li>
   );
 };
--- src/pages/Desserts.tsx
+++ src/pages/Desserts.tsx
@@ -1,17 +1,22 @@
 import Dessert from "../components/dessert/Dessert";
 import  {desserts} from "./../helpers/dessertsList";
 
+export interface DessertItem {
+    title: string;
+    img: string;
+  }
+
 const Desserts = () => {
     return (
          <main className="section">
             <div className="container">
         <h2 className="title-1">Desserts</h2>
         <ul className="desserts">
-            {/* {desserts.map(()=>{
-                return <Dessert />;
-            })} */}
-            {/* {console.log(desserts)} */}
-            <Dessert title="Best Warsaw Mochi" />
+
+            {desserts.map((dessert:DessertItem)=>{
+                return <Dessert title={dessert.title} img ={dessert.img} />;
+            })}
+            
         </ul>
     </div>
 </main>
