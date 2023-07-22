68ed445e7fe41b01df7d06db95b4da3c19726fa5
akavaliova <kovaleva89alena@gmail.com>
2023-07-22 13:09:12 +0200

    Add ScrollToTop function, so that the scrollbar always returns to the top after switching between tabs

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,13 @@ import CertainDessert from "./pages/CertainDessert";
 import Contacts from "./pages/Contacts";
 import Home from "./pages/Home";
 import Desserts from "./pages/Desserts";
+import ScrollToTop from "./utils/scrollToTop";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <ScrollToTop />
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
