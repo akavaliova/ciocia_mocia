15be46828d44a60226893cbfdb48d81555c5e63b
akavaliova <kovaleva89alena@gmail.com>
2023-07-22 12:56:19 +0200

    Applied NavLinks to the Navbar component and made them active

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,15 +1,22 @@
 import "./style.scss";
 import sun from "./../../img/icons/sun.svg";
 import moon from "./../../img/icons/moon.svg";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
+
+  const activeLink = 'nav-list__link nav-list__link--active';
+  const normalLink = 'nav-list__link';
+
+
+
   return (
     <nav className="nav">
       <div className="container">
         <div className="nav-row">
-          <a href="./index.html" className="logo">
+          <NavLink to="/" className="logo">
             <strong>ciocia_mocia</strong> natural desserts
-          </a>
+          </NavLink>
 
           <button className="dark-mode-btn">
             <img src={sun} alt="Light mode" className="dark-mode-btn__icon" />
@@ -18,22 +25,19 @@ const Navbar = () => {
 
           <ul className="nav-list">
             <li className="nav-list__item">
-              <a
-                href="./index.html"
-                className="nav-list__link nav-list__link--active"
-              >
+              <NavLink to="/" className={({isActive}) => isActive ? activeLink : normalLink}>
                 Home
-              </a>
+              </NavLink>
             </li>
             <li className="nav-list__item">
-              <a href="./projects.html" className="nav-list__link">
+              <NavLink to="/desserts" className={({isActive}) => isActive ? activeLink : normalLink}>
                 Desserts
-              </a>
+              </NavLink>
             </li>
             <li className="nav-list__item">
-              <a href="./contacts.html" className="nav-list__link">
+              <NavLink to="/contacts" className={({isActive}) => isActive ? activeLink : normalLink}>
                 Contacts
-              </a>
+              </NavLink>
             </li>
           </ul>
         </div>
